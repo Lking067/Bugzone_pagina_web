bcadde3c9d24ca0846c33a6bf3ab5b0c641442f1
Lking067 <josuelucas174@gmail.com>
2026-08-03 22:56:42 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,65 @@
+// ===============================
+// MENU LATERAL
+// ===============================
+
 function toggleMenu() {
-    document.getElementById("sideMenu").classList.toggle("active");
+
+    let menu = document.getElementById("sideMenu");
+
+    menu.classList.toggle("active");
+
+}
+
+
+// CERRAR MENU AL HACER CLICK FUERA
+
+document.addEventListener("click", function(event){
+
+    let menu = document.getElementById("sideMenu");
+    let boton = document.querySelector(".menu-btn");
+
+
+    if(
+        menu.classList.contains("active") &&
+        !menu.contains(event.target) &&
+        !boton.contains(event.target)
+    ){
+
+        menu.classList.remove("active");
+
+    }
+
+});
+
+
+
+// ===============================
+// BUSCADOR
+// ===============================
+
+function buscar(){
+
+    let texto = document.querySelector(".buscador input").value.toLowerCase();
+
+
+    let tarjetas = document.querySelectorAll(".card, .news-card, .guia-card");
+
+
+    tarjetas.forEach(function(card){
+
+        let contenido = card.innerText.toLowerCase();
+
+
+        if(contenido.includes(texto)){
+
+            card.style.display="flex";
+
+        }else{
+
+            card.style.display="none";
+
+        }
+
+    });
+
 }
