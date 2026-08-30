83740dfd444b6374e08bfbc73f172b48fb965ebe
Lking067 <josuelucas174@gmail.com>
2026-08-30 18:54:40 -0300

    Update script.js

--- script.js
+++ script.js
@@ -127,3 +127,27 @@ document.getElementById("formOpinion").addEventListener("submit", function(event
     }
 
 });
+function actualizarFechaHora() {
+    const ahora = new Date();
+
+    const fecha = ahora.toLocaleDateString('es-AR', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric'
+    });
+
+    const hora = ahora.toLocaleTimeString('es-AR', {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit'
+    });
+
+    document.getElementById("fecha-hora").textContent =
+        fecha + " | " + hora;
+}
+
+// Mostrar inmediatamente
+actualizarFechaHora();
+
+// Actualizar cada segundo
+setInterval(actualizarFechaHora, 1000);
